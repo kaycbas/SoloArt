2da2c6ab50f2c75365ec4406c2c220dc5d7107fc
Kevin Bastoul <kaycbas@gmail.com>
2021-02-25 17:21:42 -0800

    setup

---
+++ app.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+
+// app.use(express.json());
+
+app.get('/', (req, res) => res.json({
+    message: 'Hello World!'
+}))
+
+const port = process.env.PORT || 5000;
+app.listen(port, () => console.log(`Server is running on port ${port}`));
